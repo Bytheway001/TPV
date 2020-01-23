982fa3fc4a38420b66ba0c6bbd7f8f02168a3389
Rafael Castillo <Bytheway0708@gmail.com>
2020-01-23 16:46:06 -0400

    Updates

---
+++ precache-manifest.d4c40ff4073af58da10b1d64a2fbe7b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97f3619e59255ba09afacfffee39f75",
+    "url": "/TPV/index.html"
+  },
+  {
+    "revision": "9ee7c5002575d1836207",
+    "url": "/TPV/static/css/main.8bba3609.chunk.css"
+  },
+  {
+    "revision": "cfbf3846cbee925411f3",
+    "url": "/TPV/static/js/2.562b9b3e.chunk.js"
+  },
+  {
+    "revision": "b6e491a7d3935151fdd5ae48e1d7b265",
+    "url": "/TPV/static/js/2.562b9b3e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ee7c5002575d1836207",
+    "url": "/TPV/static/js/main.a248e515.chunk.js"
+  },
+  {
+    "revision": "a2117b30866b171bf1b6",
+    "url": "/TPV/static/js/runtime-main.bb8f97e0.js"
+  }
+]);
